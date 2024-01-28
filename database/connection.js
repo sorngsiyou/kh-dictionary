f440021b104d64b17251dce4aa7f6ab07d29d2f5
sorngsiyou <sorngsiyou@gmail.com>
2024-01-28 23:17:53 +0700

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sorngsiyou:rHwz2e4vphZhqyQx@cluster0.6tyefgy.mongodb.net/dictionarydb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://sorngsiyou:rHwz2e4vphZhqyQx@cluster0.6tyefgy.mongodb.net/dictionarydb?retryWrites=true&w=majority', { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
